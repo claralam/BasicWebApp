82b6197ea869f6d855691dbbceee50c6d79a4df1
claralam <claralam@andrew.cmu.edu>
2022-09-20 19:02:32 -0400

    adding change to java code to test build

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,6 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!</h1>" +
+                "<h2>Testing a change </h2>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
